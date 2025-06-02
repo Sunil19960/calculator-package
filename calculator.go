b105573ac1026937d15527e4a2878681e9264859
R SUNIL <sunilravi16@gmail.com>
2025-06-02 15:17:04 +0530

    added comments for functions

--- calculator.go
+++ calculator.go
@@ -7,13 +7,14 @@ import (
 // Add returns the sum of two float64 numbers.
 //
 // It takes two float64 values as input and returns their sum along with an error value.
+//
 // Example usage:
+//
 // result, err := Add(3.5, 2.5)
 //
 //	if err != nil {
 //	    fmt.Println("Error:", err)
 //	} else {
-//
 //	    fmt.Println("Result:", result)
 //	}
 //
@@ -26,13 +27,14 @@ func Add(firstVal, secondVal float64) (float64, error) {
 //
 // It takes two float64 values as input and returns their difference along with an error value.
 // It does not handle any specific error cases, so it always returns nil for the error.
+//
 // Example usage:
+//
 // result, err := Subtract(10.5, 2.3)
 //
 //	if err != nil {
 //	    fmt.Println("Error:", err)
 //	} else {
-//
 //	    fmt.Println("Result:", result)
 //	}
 //
@@ -45,12 +47,12 @@ func Subtract(firstVal, secondVal float64) (float64, error) {
 //
 // It takes two float64 values as input and returns their product along with an error value.
 // Example usage:
+//
 // result, err := Multiply(3.0, 4.0)
 //
 //	if err != nil {
 //	    fmt.Println("Error:", err)
 //	} else {
-//
 //	    fmt.Println("Result:", result)
 //	}
 //
@@ -60,15 +62,17 @@ func Multiply(firstVal, secondVal float64) (float64, error) {
 }
 
 // Divide returns the quotient of two float64 numbers.
+//
 // It takes two float64 values as input and returns their quotient along with an error value.
 // If the second value is zero, it returns an error indicating division by zero.
+//
 // Example usage:
+//
 // result, err := Divide(10.0, 2.0)
 //
 //	if err != nil {
 //	    fmt.Println("Error:", err)
 //	} else {
-//
 //	    fmt.Println("Result:", result)
 //	}
 //
@@ -81,15 +85,17 @@ func Divide(firstVal, secondVal float64) (float64, error) {
 }
 
 // Modulus returns the modulus of two float64 numbers.
+//
 // It takes two float64 values as input and returns the modulus along with an error value.
 // If the second value is zero, it returns an error indicating division by zero.
+//
 // Example usage:
+//
 // result, err := Modulus(10.0, 3.0)
 //
 //	if err != nil {
 //	    fmt.Println("Error:", err)
 //	} else {
-//
 //	    fmt.Println("Result:", result)
 //	}
 //
@@ -103,13 +109,14 @@ func Modulus(firstVal, secondVal float64) (float64, error) {
 
 // Square returns the square of a float64 number.
 // It takes a float64 value as input and returns its square along with an error value.
+//
 // Example usage:
+//
 // result, err := Square(4.0)
 //
 //	if err != nil {
 //	    fmt.Println("Error:", err)
 //	} else {
-//
 //	    fmt.Println("Result:", result)
 //	}
 //
