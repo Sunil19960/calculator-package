c0c61a55f43dc969bdbff734e1c20db5fd7637e8
R SUNIL <sunilravi16@gmail.com>
2025-06-02 11:22:01 +0530

    .

--- calculator.go
+++ calculator.go
@@ -21,10 +21,3 @@ func Divide(firstVal, secondVal float64) (float64, error) {
 	}
 	return firstVal / secondVal, nil
 }
-
-func Modulus(firstVal, secondVal float64) (float64, error) {
-	if secondVal == 0 {
-		return 0, fmt.Errorf("division by zero")
-	}
-	return float64(int(firstVal) % int(secondVal)), nil
-}
