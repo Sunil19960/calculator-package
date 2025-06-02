654d192fc869227c16779c49d548bdc63422dc0d
R SUNIL <sunilravi16@gmail.com>
2025-06-02 11:06:21 +0530

    restructured code

--- calculator.go
+++ calculator.go
@@ -4,21 +4,20 @@ import (
 	"fmt"
 )
 
-func Calculator(operation string, firstVal, secondVal float64) (float64, error) {
-	switch operation {
-	case "add":
-		return firstVal + secondVal, nil
-	case "subtract":
-		return firstVal - secondVal, nil
-	case "multiply":
-		return firstVal * secondVal, nil
-	case "divide":
-		if secondVal == 0 {
-			return 0, fmt.Errorf("division by zero")
-		}
-		return firstVal / secondVal, nil
-	default:
-		return 0, fmt.Errorf("unknown operation: %s", operation)
-	}
+func Add(firstVal, secondVal float64) (float64, error) {
+	return firstVal + secondVal, nil
+}
 
+func Subtract(firstVal, secondVal float64) (float64, error) {
+	return firstVal - secondVal, nil
+}
+
+func Multiply(firstVal, secondVal float64) (float64, error) {
+	return firstVal * secondVal, nil
+}
+func Divide(firstVal, secondVal float64) (float64, error) {
+	if secondVal == 0 {
+		return 0, fmt.Errorf("division by zero")
+	}
+	return firstVal / secondVal, nil
 }
